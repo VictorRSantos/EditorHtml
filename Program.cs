5c15695d92966f9b799606dc52c7431df75a9061
Victor Rodrigues dos Santos <vrsisa@hotmail.com>
2022-01-01 14:10:23 -0300

    First Commit - Iniciando Projeto

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using EditorHtml;
+
+namespace EditorHtmk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu.Show();
+        }
+    }
+}
